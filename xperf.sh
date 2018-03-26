61949a1e390128aa04df4ff79541e0b51ecf8995
Xiaofeng Wang <henrywangxf@me.com>
2018-03-26 10:48:44 +0800

    Optimize xperf.sh.

--- xperf.sh
+++ xperf.sh
@@ -9,70 +9,90 @@ STOP_IPERF="killall iperf >/dev/null 2>&1 || true"
 . workload.conf
 . xperf.conf
 
-ExecCommandOnClient() {
-    ssh -q -i ${SSH_PRIVATE_KEY} ${IPERF_USER}@${CLIENT_IP} $1
-    return $?
+ExecCommandOverSSH() {
+    case $1 in
+        "client")
+            ssh -q -i ${SSH_PRIVATE_KEY} ${IPERF_USER}@${CLIENT_IP} $2
+            return $?
+            ;;
+        "server")
+            ssh -q -i ${SSH_PRIVATE_KEY} ${IPERF_USER}@${SERVER_IP} $2
+            return $?
+            ;;
+        *)
+            echo -e "Sorry, I can not get what you want"
+            return 1
+            ;;
+    esac
 }
 
-ExecCommandOnServer() {
-    ssh -q -i ${SSH_PRIVATE_KEY} ${IPERF_USER}@${SERVER_IP} $1
-    return $?
+RunDstatOverSSH() {
+    ExecCommandOverSSH $1 "export DSTAT_TIMEFMT='%Y/%m/%d %H:%M:%S'; nohup dstat -tv --output ${RESULT_DIR}/run$2-$1-dstat.csv 10 >/dev/null 2>&1 &"
+}
+
+# e.g. RunIperfOverSSH "client" 1 "140M"
+RunIperfOverSSH() {
+    case $1 in
+        "client")
+            ExecCommandOverSSH $1 "echo \$(date +%Y%m%d_%H%M%S) | tee ${RESULT_DIR}/run$2-$3-$1.iperf && iperf -u -c ${SERVER_IP} -d -b $3 -t ${DURATION} -i ${INTERVAL} -B ${CLIENT_IP} -L ${CLIENT_PORT} -e -x CSV -p ${SERVER_PORT} -l ${UDP_PAYLOAD} | tee -a ${RESULT_DIR}/run$2-$3-$1.iperf";;
+        "server")
+            ExecCommandOverSSH $1 "nohup iperf -s -i ${INTERVAL} -u -e -p ${SERVER_PORT} -x CSV > ${RESULT_DIR}/run$2-$3-$1.iperf &";;
+        *)
+            echo -e "Sorry, I can not get what you want"
+            return 1
+            ;;
+    esac
 }
 
 # clear result folder if exists
-ExecCommandOnClient "rm -rf ${RESULT_DIR}"
-ExecCommandOnServer "rm -rf ${RESULT_DIR}"
+ExecCommandOverSSH "client" "rm -rf ${RESULT_DIR}"
+ExecCommandOverSSH "server" "rm -rf ${RESULT_DIR}"
 
 # creat result folder
-ExecCommandOnClient "mkdir ${RESULT_DIR}"
-ExecCommandOnServer "mkdir ${RESULT_DIR}"
+ExecCommandOverSSH "client" "mkdir ${RESULT_DIR}"
+ExecCommandOverSSH "server" "mkdir ${RESULT_DIR}"
 
 # stop iperf and dstat on both sides
-ExecCommandOnClient "$STOP_IPERF"
-ExecCommandOnServer "$STOP_IPERF"
-ExecCommandOnClient "$STOP_DSTAT"
-ExecCommandOnServer "$STOP_DSTAT"
+ExecCommandOverSSH "client" "$STOP_IPERF"
+ExecCommandOverSSH "server" "$STOP_IPERF"
+ExecCommandOverSSH "client" "$STOP_DSTAT"
+ExecCommandOverSSH "server" "$STOP_DSTAT"
 
-ExecCommandOnClient "[ ! -e /usr/local/bin/dstat ]" && scp $IPERF_FILE ${IPERF_USER}@${CLIENT_IP}:/usr/local/bin
-ExecCommandOnServer "[ ! -e /usr/local/bin/dstat ]" && scp $IPERF_FILE ${IPERF_USER}@${SERVER_IP}:/usr/local/bin
+ExecCommandOverSSH "client" "[ ! -e /usr/local/bin/dstat ]" && scp $IPERF_FILE ${IPERF_USER}@${CLIENT_IP}:/usr/local/bin
+ExecCommandOverSSH "server" "[ ! -e /usr/local/bin/dstat ]" && scp $IPERF_FILE ${IPERF_USER}@${SERVER_IP}:/usr/local/bin
 
-ExecCommandOnClient "[ ! -e /usr/local/bin/dstat ]" && scp $DSTAT_FILE ${IPERF_USER}@${CLIENT_IP}:/usr/local/bin
-ExecCommandOnServer "[ ! -e /usr/local/bin/dstat ]" && scp $DSTAT_FILE ${IPERF_USER}@${SERVER_IP}:/usr/local/bin
+ExecCommandOverSSH "client" "[ ! -e /usr/local/bin/dstat ]" && scp $DSTAT_FILE ${IPERF_USER}@${CLIENT_IP}:/usr/local/bin
+ExecCommandOverSSH "server" "[ ! -e /usr/local/bin/dstat ]" && scp $DSTAT_FILE ${IPERF_USER}@${SERVER_IP}:/usr/local/bin
 
 for ((i=1; i<=${ITERATIONS}; i++)); do
     # stop dstat on both sides
-    ExecCommandOnClient "$STOP_DSTAT"
-    ExecCommandOnServer "$STOP_DSTAT"
+    ExecCommandOverSSH "client" "$STOP_DSTAT"
+    ExecCommandOverSSH "server" "$STOP_DSTAT"
     sleep 1
 
-    DSTAT_CLIENT="export DSTAT_TIMEFMT='%Y/%m/%d %H:%M:%S'; nohup dstat -tv --output ${RESULT_DIR}/run${i}-client-dstat.csv 10 >/dev/null 2>&1 &"
-    DSTAT_SERVER="export DSTAT_TIMEFMT='%Y/%m/%d %H:%M:%S'; nohup dstat -tv --output ${RESULT_DIR}/run${i}-server-dstat.csv 10 >/dev/null 2>&1 &"
     echo -e "Run${i}: start dstat on client and server.\n-----------------"
-    ExecCommandOnClient "$DSTAT_CLIENT"
-    ExecCommandOnServer "$DSTAT_SERVER"
+    RunDstatOverSSH "client" $i
+    RunDstatOverSSH "server" $i
     
     for bandwidth in ${LOAD[*]}; do
         echo -e "Run${i}: start iperf on ${LOAD[*]}\n-----------------"
-        IPERF_CLIENT="echo \$(date +%Y%m%d_%H%M%S) | tee ${RESULT_DIR}/run${i}-${bandwidth}-client.iperf && iperf -u -c ${SERVER_IP} -d -b ${bandwidth} -t ${DURATION} -i ${INTERVAL} -B ${CLIENT_IP} -L 6001 -e -x CSV -p 5001 -l 104 | tee -a ${RESULT_DIR}/run${i}-${bandwidth}-client.iperf"
-        IPERF_SERVER="nohup iperf -s -i 10 -u -e -p 5001 -x CSV > ${RESULT_DIR}/run${i}-${bandwidth}-server.iperf &"
-
         # stop iperf
-        ExecCommandOnClient "$STOP_IPERF"
-        ExecCommandOnServer "$STOP_IPERF"
+        ExecCommandOverSSH "client" "$STOP_IPERF"
+        ExecCommandOverSSH "server" "$STOP_IPERF"
         sleep 1
 
         echo -e "Run${i}: start iperf on server.\n-----------------"
-        ExecCommandOnServer "$IPERF_SERVER"
+        RunIperfOverSSH "server" $i $bandwidth
         sleep 1
 
         echo -e "Run${i}: ${bandwidth} load.\n-----------------"
-        ExecCommandOnClient "$IPERF_CLIENT"
+        RunIperfOverSSH "client" $i $bandwidth
 
         # stop iperf
-        ExecCommandOnServer "$STOP_IPERF"
+        ExecCommandOverSSH "server" "$STOP_IPERF"
     done
 
     # stop dstat
-    ExecCommandOnClient "$STOP_DSTAT"
-    ExecCommandOnServer "$STOP_DSTAT"
+    ExecCommandOverSSH "client" "$STOP_DSTAT"
+    ExecCommandOverSSH "server" "$STOP_DSTAT"
 done
